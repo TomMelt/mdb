07c83a256a2e2cdb737e0f27ea53ece9c6e98a75
melt <thomas.meltzer1@gmail.com>
2023-12-18 17:43:45 +0000

    fix mdb launch args handler
    I hadn't previously tried running with args. This change allows me to
    actually run with arguments (especially those containing hyphens `-`)

--- mdb/mdb_launch.py
+++ mdb/mdb_launch.py
@@ -1,5 +1,6 @@
-import subprocess as sub
 import os
+import subprocess as sub
+
 import click
 
 
@@ -11,12 +12,14 @@ import click
     show_default=True,
     help="Starting port address. Each rank's port is assigned as [port_address + rank].",
 )
-@click.option(
-    "--args",
-    help="program and any command line arguments. Similar to gdb option.",
+@click.argument(
+    "args",
+    # help="program and any command line arguments. Similar to gdb option.",
     required=True,
+    nargs=-1,
 )
 def launch(port, args):
+    args = list(args)
     prog_opts = dict(port=port, args=args)
 
     try:
@@ -30,6 +33,6 @@ def launch(port, args):
 
 def launch_server(my_rank, no_ranks, port, args):
     port = port + int(my_rank)
-    sub.run(["gdbserver", f"localhost:{port}", f"{args}"])
+    sub.run(["gdbserver", f"localhost:{port}"] + args)
     print(f"server on rank {my_rank} closed")
     return
