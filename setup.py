cea6e46892d8f70bcb78b414178c5ac1960b4851
melt <thomas.meltzer1@gmail.com>
2024-01-01 23:13:29 +0000

    simplify CI workflow and setup.py

--- setup.py
+++ setup.py
@@ -6,14 +6,17 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-        "Click",
-        "Pexpect",
-        "Matplotlib",
-        "Numpy",
+        "click",
+        "matplotlib",
+        "numpy",
+        "pexpect",
     ],
     extras_require={
         "develop": [
-            "Mypy",
+            "black",
+            "flake8",
+            "mypy",
+            "types-setuptools",
         ]
     },
     entry_points={
