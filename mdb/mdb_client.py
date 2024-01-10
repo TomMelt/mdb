9cc705e2aac727ddbb20fea003bd406882999f73
melt <thomas.meltzer1@gmail.com>
2024-01-10 11:57:47 +0000

    add rich progress bar for connections

--- mdb/mdb_client.py
+++ mdb/mdb_client.py
@@ -7,6 +7,7 @@ from multiprocessing.dummy import Pool
 from typing import TYPE_CHECKING
 
 import pexpect  # type: ignore
+from rich.progress import BarColumn, MofNCompleteColumn, Progress, TextColumn
 
 from .mdb_shell import GDBPROMPT
 from .utils import parse_ranks, strip_bracketted_paste, strip_control_characters
@@ -31,9 +32,17 @@ class Client:
     def close_procs(self) -> None:
         """Close all open processes."""
 
-        for proc in self.dbg_procs:
-            print(f"closing process {proc.pid}")
-            proc.close()
+        with Progress(
+            TextColumn("[progress.description]{task.description}"),
+            BarColumn(),
+            MofNCompleteColumn(),
+        ) as progress:
+            connect_progress = progress.add_task(
+                "Closing processes...", total=self.ranks
+            )
+            for proc in self.dbg_procs:
+                proc.close()
+                progress.advance(connect_progress)
         return
 
     def clear_stdout(self) -> None:
@@ -53,12 +62,17 @@ class Client:
 
     def connect(self) -> None:
         """Connect to gdb server on each rank."""
-        try:
-            procs = self.pool.map(self.connect_proc, list(range(self.ranks)))
-            for p in procs:
-                self.dbg_procs.append(p)
-        except TimeoutError as e:
-            raise TimeoutError(e)
+        with Progress(
+            TextColumn("[progress.description]{task.description}"),
+            BarColumn(),
+            MofNCompleteColumn(),
+        ) as progress:
+            connect_progress = progress.add_task(
+                "Connecting processes...", total=self.ranks
+            )
+            for proc in self.pool.imap(self.connect_proc, list(range(self.ranks))):
+                progress.advance(connect_progress)
+                self.dbg_procs.append(proc)
         return
 
     def connect_proc(self, rank: int) -> pexpect.spawn:
@@ -72,10 +86,8 @@ class Client:
             gdb server.
         """
         port: int = self.start_port + rank
-        print(f"connecting to port: {port}")
         c = pexpect.spawn("gdb -q", timeout=None)
         c.expect(GDBPROMPT)
-        print(c.before.decode("utf-8"), end="")
         c.sendline("set pagination off")
         c.expect(GDBPROMPT)
         c.sendline("set confirm off")
