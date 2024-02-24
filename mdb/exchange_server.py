70fd5fc23724921fe58a32bc808d885240cf7c12
fjebaker <fergusbkr@gmail.com>
2024-02-24 13:32:38 +0000

    feat: make the exchange server dumber
    Previously the exchange server was dumb, but it still was smart enough
    to know that every message from the client must result in a message back
    from the debuggers. This modicum of intelligence meant it was deaf to
    the client sending additional messages, such as interrupts, until the
    debuggers replied to the first message.
    
    Exchange server now sends whatever it receives from the debuggers to the
    clients, and whatever it receives from the clients to the debuggers
    without trying to verify the order.

--- mdb/exchange_server.py
+++ mdb/exchange_server.py
@@ -41,34 +41,59 @@ class AsyncExchangeServer:
         connection_type = await conn.handle_connection()
         logger.info(f"exchange server received a {connection_type} connection.")
 
+        # checkhov's loop
+        loop = asyncio.get_event_loop()
+
         # here you'd distinguish the connection too, to work out if it needs
         # to be pushed to `self.servers` or not, etc
         if connection_type == "debug":
             self.servers.append(conn)
             return  # keep connection open
 
-        async def temp(server):
-            await server.send_message(command)
-            message = await server.recv_message()
-            return message
-
         if connection_type == "client":
+            # tell the client about the setup
             message = {
                 "ranks": self.number_of_ranks,
                 "backend": self.backend,
             }
             await conn.send_message(message)
-            while True:
-                command = await conn.recv_message()
-                tasks = [asyncio.create_task(temp(server)) for server in self.servers]
-                print("waiting for output...")
-                output = await asyncio.gather(*tasks)
-                print("Output received...")
-                response = {"result": output}
-                await conn.send_message(response)
+            # schedule the loop to run
+            loop.create_task(self.client_loop(conn))
+            # but allow this function to return so it's not just stuck on the
+            # stack
+            return
 
         # do this incase we somehow fall through
         conn.writer.close()
+
+    async def _forward_all_debuggers_to_client(self, conn):
+        while True:
+            tasks = [
+                asyncio.create_task(debugger.recv_message())
+                for debugger in self.servers
+            ]
+            output = await asyncio.gather(*tasks)
+            response = {"result": output}
+            logger.debug("Sending results to client")
+            await conn.send_message(response)
+
+    async def client_loop(self, conn):
+        # the problem here is we don't know if another message is going to come
+        # from the client before the debugger has had the time to send
+        # something back, and we can't assume all sends will be followed by
+        # receives in order so instead. It is valid for the client to make that
+        # assumption, but not for the exchange server
+
+        # to handle this, every time a message comes in from the client, we send it to all debuggers
+        # every time a message comes in from the debuggers, we send it to the client
+
+        asyncio.create_task(self._forward_all_debuggers_to_client(conn))
+
+        while True:
+            command = await conn.recv_message()
+            logger.debug("Received from client: %s", command)
+            for debugger in self.servers:
+                await debugger.send_message(command)
 
     def start_server(self):
         task = asyncio.start_server(
