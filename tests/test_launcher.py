caa3963641e49c6a18ed5b09566ca1c45b947d97
fjebaker <fergusbkr@gmail.com>
2024-05-04 10:36:35 +0100

    test: use slow_down_tests in ping to do cleanup

--- tests/test_launcher.py
+++ tests/test_launcher.py
@@ -4,6 +4,19 @@ from mdb.mdb_client import Client
 
 import asyncio
 import time
+import os
+
+# todo: i forget how to do it but there's a
+# universal pytest file you can define which
+# contains fixtures to run for every test
+@pytest.fixture(autouse=True)
+def slow_down_tests() -> Generator[None, None, None]:
+    yield
+    sleep(1)
+    run(
+        shlex.split("pkill -9 mdb"),
+        capture_output=True,
+    )
 
 def test_ping_launcher() -> None:
     launch_command = "mdb launch -b gdb -t examples/simple-mpi-cpp.exe -n 2 -h 127.0.0.1 --log-level=DEBUG -p 62000"
