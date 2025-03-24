ef8e803763a96ce14fb609a6c13031bb1f68c6a4
armanaax <ffilomath@gmail.com>
2025-03-24 12:14:19 +0100

    feat: add plugin-based debugger backends, refactor gdb & lldb

---
+++ src/mdb/plugins/lldb.py
@@ -0,0 +1,31 @@
+from mdb.backend import DebugBackend
+
+
+class LLDBBackend(DebugBackend):
+    @property
+    def name(self) -> str:
+        return "lldb"
+
+    @property
+    def debug_command(self) -> str:
+        return "lldb --source-quietly --no-use-colors"
+
+    @property
+    def argument_separator(self) -> str:
+        return "--"
+
+    @property
+    def prompt_string(self) -> str:
+        return r"\(lldb\)"
+
+    @property
+    def default_options(self) -> list[str]:
+        return ["b main"]
+
+    @property
+    def start_command(self) -> str:
+        return "run"
+
+    @property
+    def float_regex(self) -> str:
+        return r"\d+ = ([+-]?(\d+(\.\d*)?|\.\d+)([eE][+-]?\d+)?)"
