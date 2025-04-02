45223542fd697d046f5b7429f7a242aca8ac7506
armanaax <ffilomath@gmail.com>
2025-04-02 18:07:24 +0200

    feat: add cuda-gdb plugin

---
+++ src/mdb/plugins/cuda-gdb.py
@@ -0,0 +1,33 @@
+from mdb.backend import DebugBackend
+
+
+class CudaGDBBackend(DebugBackend):
+
+    @property
+    def name(self) -> str:
+        return "cuda-gdb"
+
+    @property
+    def debug_command(self) -> str:
+        return "cuda-gdb -q"
+
+    @property
+    def argument_separator(self) -> str:
+        return "--args"
+
+    @property
+    def prompt_string(self) -> str:
+        return r"\(cuda-gdb\)"
+
+    @property
+    def default_options(self) -> list[str]:
+        commands = ["set pagination off", "set confirm off"]
+        return commands
+
+    @property
+    def start_command(self) -> str:
+        return "start"
+
+    @property
+    def float_regex(self) -> str:
+        return r"\d+ = ([+-]?(\d+(\.\d*)?|\.\d+)([eE][+-]?\d+)?)"
