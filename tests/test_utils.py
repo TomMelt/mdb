57707adb9034163657f83525ba95a4190fd08b1a
tommelt <thomas.meltzer1@gmail.com>
2026-03-13 05:06:27 -0500

    chore: update copyright dates

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,4 +1,4 @@
-# Copyright 2023-2024 Tom Meltzer. See the top-level COPYRIGHT file for
+# Copyright 2023-2026 Tom Meltzer. See the top-level COPYRIGHT file for
 # details.
 
 from mdb.utils import parse_ranks, strip_bracketted_paste, strip_control_characters
